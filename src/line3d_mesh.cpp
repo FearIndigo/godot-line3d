717d828d9b0292da716dac6f9124b63c76b1d927
JoeMcCleery <mccleery.joseph@gmail.com>
2024-08-29 17:02:45 +0800

    Add m_points methods to line3dmesh

--- src/line3d_mesh.cpp
+++ src/line3d_mesh.cpp
@@ -18,14 +18,58 @@ Line3DMesh::~Line3DMesh() {
 	// Add your cleanup here.
 }
 
-void Line3DMesh::set_points(const PackedVector3Array &p_points) {
-	m_points = p_points;
+void Line3DMesh::add_point(const Vector3 &p_position, int64_t p_index = -1) {
+	if(p_index == -1) p_index = m_points.size();
+	m_points.insert(p_index, p_position);
+}
+
+void Line3DMesh::clear_points() {
+	m_points.clear();
+}
+
+Vector3 Line3DMesh::get_point_position(int64_t p_index) const {
+	return m_points[p_index];
 }
 
 PackedVector3Array Line3DMesh::get_points() const {
 	return m_points;
 }
 
+void Line3DMesh::remove_point(int64_t p_index) {
+	m_points.remove_at(p_index);
+}
+
+void Line3DMesh::set_point_position(int64_t p_index, const Vector3 &p_position) {
+	m_points.set(p_index, p_position);
+}
+
+void Line3DMesh::set_points(const PackedVector3Array &p_points) {
+	m_points = p_points;
+}
+
 void Line3DMesh::redraw() {
-	// Redraw mesh here.
+	// Clear mesh.
+	clear_surfaces();
+
+	if(m_points.is_empty()) return;
+
+	// Begin draw.
+	surface_begin(PRIMITIVE_TRIANGLES);
+
+	// Prepare attributes for add_vertex.
+	surface_set_normal(Vector3(0, 0, 1));
+	surface_set_uv(Vector2(0, 0));
+	// Call last for each vertex, adds the above attributes.
+	surface_add_vertex(Vector3(-1, -1, 0));
+
+	surface_set_normal(Vector3(0, 0, 1));
+	surface_set_uv(Vector2(0, 1));
+	surface_add_vertex(Vector3(-1, 1, 0));
+
+	surface_set_normal(Vector3(0, 0, 1));
+	surface_set_uv(Vector2(1, 1));
+	surface_add_vertex(Vector3(1, 1, 0));
+
+	// End drawing.
+	surface_end();
 }
--- src/line3d_mesh.h
+++ src/line3d_mesh.h
@@ -19,8 +19,14 @@ public:
 	Line3DMesh();
 	~Line3DMesh();
 
-	void set_points(const PackedVector3Array &p_points);
+	// m_points methods
+	void add_point(const Vector3 &p_position, int64_t p_index = -1);
+	void clear_points();
+	Vector3 get_point_position(int64_t p_index) const;
 	PackedVector3Array get_points() const;
+	void remove_point(int64_t p_index);
+	void set_point_position(int64_t p_index, const Vector3 &p_position);
+	void set_points(const PackedVector3Array &p_points);
 
 	void redraw();
 };

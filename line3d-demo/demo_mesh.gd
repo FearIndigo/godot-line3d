41c2245046f44fa264fa0da1f3f4254c973e1d66
JoeMcCleery <mccleery.joseph@gmail.com>
2024-08-31 13:44:18 +0800

    Add demo_mesh.gd script and remove example_line_3d_mesh.tres

---
+++ line3d-demo/demo_mesh.gd
@@ -0,0 +1,18 @@
+class_name DemoMesh
+
+extends MeshInstance3D
+
+@export_range(0.01, 10) var speed: float = 1.0
+var time: float = 0.0
+
+func _init() -> void:
+	mesh = Line3DMesh.new()
+	mesh.points = [Vector3(0,0,0), Vector3(0,1,0)]
+
+func _process(delta: float) -> void:
+	var point = mesh.get_point_position(1)
+	point.x = sin(time)
+	mesh.set_point_position(1, point)
+	# Call redraw() to apply changes to the mesh.
+	mesh.redraw()
+	time += delta * speed

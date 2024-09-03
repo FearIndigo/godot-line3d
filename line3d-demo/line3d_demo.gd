dccf678ffc938ee64b76adf828b964cae84e3307
JoeMcCleery <mccleery.joseph@gmail.com>
2024-09-03 16:56:37 +0800

    Update demo scripts

--- line3d-demo/line3d_demo.gd
+++ line3d-demo/line3d_demo.gd
@@ -2,10 +2,11 @@ class_name Line3DDemo
 
 extends Line3D
 
-@export var speed: float = 2.0
+@export var speed: float = 3.0
 @export var num_points: int = 20
 @export var line_length: float = 10.0
 @export var frequency: float = 5.0
+@export var magnitude: float = 0.5
 var time: float = 0.0
 
 func _init() -> void:
@@ -16,7 +17,8 @@ func _init() -> void:
 func _process(delta: float) -> void:
 	for i in range(points.size()):
 		var point = get_point_position(i)
-		point.x = sin(time + float(i) / num_points * frequency)
+		var t: float = float(i) / (num_points - 1)
+		point.x = sin(time + t * frequency) * magnitude
 		set_point_position(i, point)
 	
 	time += delta * speed
